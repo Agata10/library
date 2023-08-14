d6573111e2769f57468bec320fe627eb3224a72b
Agata Deter <124964478+Agata10@users.noreply.github.com>
2023-08-14 14:02:33 -0600

    Add function to add the book

---
+++ javascript.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${read} yet`;
+  }
+}
+
+function addBookToLibrary() {
+  const book1 = new Book("The Hobbit", "J.R talkin", 295, "read");
+  console.log(book1.info());
+}
+addBookToLibrary();
