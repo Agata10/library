8a1a4ff00e4001abdf249f8922714197f401f965
Agata Deter <124964478+Agata10@users.noreply.github.com>
2023-08-15 20:45:28 -0600

    Fix issue with adding book from dialog

--- javascript.js
+++ javascript.js
@@ -1,11 +1,3 @@
-let myLibrary = [];
-let table = document.getElementById("table")
-let tableBody = table.getElementsByTagName("tbody")[0];
-const book1 = new Book("https://m.media-amazon.com/images/I/71HbYElfY0L._AC_UF1000,1000_QL80_.jpg", "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, "read");
-const book2 = new Book("https://m.media-amazon.com/images/I/61Nv1wDO91L._SX281_BO1,204,203,200_.jpg", "Breaking Creed", "Alex Kava", 368, "not read");
-myLibrary.push(book1);
-myLibrary.push(book2);
-
 
 function Book(img, title, author, pages, read) {
   this.img = img;
@@ -28,7 +20,50 @@ function createCancelBtn() {
 
 }
 
-function addBookToLibrary() {
+function addUserBook() {
+  const addBookBtn = document.getElementById("add-button");
+  const favDialog = document.getElementById("favDialog");
+  const outputBox = document.querySelector("output");
+  const submitForm = favDialog.querySelector("#my-form");
+  const submitBookBtn = favDialog.querySelector("#submitBtn");
+  let urlImg = favDialog.querySelector("#url");
+  const bookTitle = favDialog.querySelector("#title");
+  const bookAuthor = favDialog.querySelector("#author");
+  const bookPages = document.getElementById("pages");
+  const bookReadStatus = document.getElementById("read-check");
+ 
+  
+  // "Show the dialog" button opens the <dialog> modally
+  addBookBtn.addEventListener("click", () => {
+    favDialog.showModal();
+  });
+
+  //close dialog when outside click of form
+  favDialog.addEventListener('click', (event) => {
+    if (event.target === favDialog) {
+      favDialog.close();
+  } 
+});
+
+  submitBookBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+      
+    if(urlImg.value === ""){
+      urlImg.value = "./images/book-half.svg";
+    }
+
+    const newBook = new Book(urlImg.value, bookTitle.value, bookAuthor.value, bookPages.value, bookReadStatus.checked);
+    addBookToLibrary(newBook);
+    console.log(newBook);
+    favDialog.close();
+  });
+}
+
+function addBookToLibrary(book) {
+  let myLibrary = [];
+  let table = document.getElementById("table")
+  let tableBody = table.getElementsByTagName("tbody")[0];
+  myLibrary.push(book);
 
   for (let book of Object.values(myLibrary)) {
     let countRow = 0;
@@ -62,6 +97,11 @@ function addBookToLibrary() {
 
   }
 }
-addBookToLibrary();
 
-  
+const book1 = new Book("https://m.media-amazon.com/images/I/71HbYElfY0L._AC_UF1000,1000_QL80_.jpg", "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, "read");
+const book2 = new Book("https://m.media-amazon.com/images/I/61Nv1wDO91L._SX281_BO1,204,203,200_.jpg", "Breaking Creed", "Alex Kava", 368, "not read");
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+addUserBook();
+
